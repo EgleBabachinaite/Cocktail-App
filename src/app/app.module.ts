3980198b20537591b897d327da1a774b36bdabc6
egle_babachinaite <eglebabachinaite123@gmail.com>
2019-05-03 00:33:18 +0300

    pushing a new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+import { CocktailListComponent } from './cocktails/cocktail-list/cocktail-list.component';
+import { CocktailItemComponent } from './cocktails/cocktail-list/cocktail-item/cocktail-item.component';
+import { CocktailDetailComponent } from './cocktails/cocktail-detail/cocktail-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailsComponent,
+    CocktailListComponent,
+    CocktailItemComponent,
+    CocktailDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
